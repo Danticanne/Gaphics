f9f0f60b7d8ed7335617bb7f43c9cf9c4e833270
Antoine H <ddanticanne@gmail.com>
2023-02-27 18:16:59 +0100

    ajout graduation

--- public/script.js
+++ public/script.js
@@ -1,9 +1,10 @@
 var nbDeBoule = 250
 var demiph = $(window).height()/2 
-
+var precisionEchelle = 10
 var taille = $(window).width()/nbDeBoule
 var id
-console.log(taille)
+
+//creation de chaque boule + placement verticale
 
 for (let i = 1; i < nbDeBoule + 1; i++) {
     jQuery
@@ -13,12 +14,39 @@ for (let i = 1; i < nbDeBoule + 1; i++) {
     })
     .css({'height': taille.toString(), 'width': taille.toString(), 'top': (taille/2 + (taille*(i-(nbDeBoule/2+0.5))) + $(window).height()/2).toString() + "px"})
     .appendTo('#container')
-
 }
 
-verticale()
+//création de l'echelle horizontale
 
-function verticale() {
+for (let i = 1; i < precisionEchelle + 1; i++) {
+    jQuery
+    ('<p>', {
+        class : 'nbEchelle',
+        id : 'nbEchelleh' + (i).toString()
+    })
+    .css({'top' : demiph.toString() + 'px', 'left' : ($(window).width()/precisionEchelle*i).toString() + 'px'})
+    .html(((i-precisionEchelle/2)*nbDeBoule/precisionEchelle).toString())
+    .appendTo('#echelle')
+}
+
+//création de l'echelle verticale
+
+for (let i = 1; i < precisionEchelle + 1; i++) {
+    jQuery
+    ('<p>', {
+        class : 'nbEchelle',
+        id : 'nbEchellev' + (i).toString()
+    })
+    .css({'left' : ($(window).width()/2).toString() + 'px', 'top' : (demiph*2/precisionEchelle*i).toString() + 'px'})
+    .html((   -((demiph*2/precisionEchelle*i-$(window).height()) + $(window).height()/2).toString()   ).toString())
+    .appendTo('#echelle')
+}
+
+//fonction pour placer les boules à l'horizontale + appel
+
+horizontale()
+
+function horizontale() {
     for (let i = 1; i < nbDeBoule + 1; i++) {
         id = '#boule' + i.toString()
         //$('#boule1').css({'top' : '50vh', 'left' : (taille/2 + (i*taille - taille)).toString() + 'px'})
@@ -32,6 +60,8 @@ function verticale() {
         })
     }
 }
+
+//fonction affine
 
 var calc
 
